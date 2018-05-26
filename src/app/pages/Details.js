f155120b98b76620738467be823650f8316557e3
freeman-tmc <freeman.tomic84@gmail.com>
2018-05-26 13:21:20 +0200

    error handling done

--- src/app/pages/Details.js
+++ src/app/pages/Details.js
@@ -1,45 +1,61 @@
 import React from 'react';
 import findLogo from '../../services/logoService';
+import defaultLogo from '../../default_logo.png';
 
 class Details extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             flightDetails: {},
-            companyLogoUrl: ''
+            companyLogoUrl: '',
+            loading: true
         }
     }
 
     componentDidMount() {
+       
         let flightData = sessionStorage.getItem('flight');
-        let flightDataObject = JSON.parse(flightData);
-        let company = flightDataObject.company;
+        if (flightData) {
+            let flightDataObject = JSON.parse(flightData);
+            let company = flightDataObject.company;
 
-        findLogo(company)
-        .then(data => {
-            this.setState({
-                flightDetails: flightDataObject,
-                companyLogoUrl: data
-            })
+            findLogo(company)
+                .then(data => {
+                    // if logo doesnt exist use default logo
+                    let logoImg = data || defaultLogo;
+                    this.setState({
+                        flightDetails: flightDataObject,
+                        companyLogoUrl: logoImg,
+                        loading: false
+                    })
 
-        })
-        // .catch(error => {
-        //     this.setState({
-        //         flightDetails: flightDataObject,
-        //         companyLogoUrl: error.message
-        //     })
-        // })
+                })
+                .catch(() => {
+                    this.setState({
+                        flightDetails: flightDataObject,
+                        companyLogoUrl: defaultLogo,
+                        loading: false
+                    })
+                })
+        } else {
+            this.props.history.push('/');
+        }
     }
 
     render() {
-        return(
+        return (
             <div>
-                <p>Manufacturer: {this.state.flightDetails.manufacturer}</p>
-                <p>Model: {this.state.flightDetails.model}</p>
-                <p>Destination: {this.state.flightDetails.destination}</p>
-                <p>Origin: {this.state.flightDetails.origin}</p>
-                <p>Company: {this.state.flightDetails.company}</p>
-                <img src={this.state.companyLogoUrl} alt=""/>
+                {!this.state.loading
+                    ? <div>
+                        <p>Manufacturer: {this.state.flightDetails.manufacturer}</p>
+                        <p>Model: {this.state.flightDetails.model}</p>
+                        <p>Origin: {this.state.flightDetails.origin}</p>
+                        <p>Destination: {this.state.flightDetails.destination}</p>
+                        <p>Company: {this.state.flightDetails.company}</p>
+                        <img src={this.state.companyLogoUrl} alt="" />
+                    </div>
+                    : <h2>Loading...</h2>
+                }
             </div>
         )
     }
--- src/app/pages/FlightList.js
+++ src/app/pages/FlightList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import getFlights from '../../services/flightService';
+import travel from '../../travel.svg';
 
 
 class FlightList extends React.Component {
@@ -8,40 +9,49 @@ class FlightList extends React.Component {
         this.state = {
             flights: [],
             refreshId: '',
-            geolocationError: false
+            geolocationError: false,
+            connectionError: false,
+            loading: true
         }
     }
 
+
     componentDidMount() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                let latitude = position.coords.latitude;
-                let longitude = position.coords.longitude;
-                const url = `http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${latitude}&lng=${longitude}&fDstL=0&fDstU=150`;
-                this.collectFlightData(url);
+
+        navigator.geolocation.getCurrentPosition(position => {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            const url = `http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=${latitude}&lng=${longitude}&fDstL=0&fDstU=150`;
+            this.collectFlightData(url);
+        },
+            () => {
+                this.setState({
+                    geolocationError: true
+                })
             })
-        } else {
-            this.setState({
-                geolocationError: true
-            })
-        }
 
     }
 
     componentWillUnmount() {
-        console.log('skidanje');
         clearTimeout(this.state.refreshId);
     }
 
     collectFlightData = (url) => {
-        console.log('counter');
+
         getFlights(url)
             .then(data => {
                 this.setState({
                     flights: data.sort(function (a, b) { return b.altitude - a.altitude }),
-                    refreshId: setTimeout(() => this.collectFlightData(url), 60000)
+                    refreshId: setTimeout(() => this.collectFlightData(url), 60000),
+                    loading: false,
                 })
             })
+            .catch(() => {
+                this.setState({
+                    connectionError: true
+                })
+            })
+
     }
 
 
@@ -56,26 +66,34 @@ class FlightList extends React.Component {
         return (
             <div>
                 {!this.state.geolocationError
-                ?
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Heading</th>
-                            <th>Altitude(ft)</th>
-                            <th>Flight number</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.flights.map((el, index) => {
-                            return <tr key={index} data={JSON.stringify(el)} onClick={this.handleClick}>
-                                <td id="jeste">{el.heading}</td>
-                                <td id="jeste">{el.altitude}</td>
-                                <td id="jeste">{el.flightNumber}</td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-                : <h1>Geolocation is not allowed!</h1>}
+                    ? !this.state.connectionError
+                        ? !this.state.loading
+                            ? this.state.flights.length
+                                ? <table>
+                                        <thead>
+                                            <tr>
+                                                <th>Heading</th>
+                                                <th>Altitude(ft)</th>
+                                                <th>Flight number</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            {this.state.flights.map((el, index) => {
+                                                return <tr key={index} data={JSON.stringify(el)} onClick={this.handleClick}>
+                                                    <td><img width="50px" src={travel} alt="" /></td>
+                                                    <td>{el.altitude}</td>
+                                                    <td>{el.flightNumber}</td>
+                                                </tr>
+                                            })}
+                                        </tbody>
+                                    </table>
+                                : <h2>There's no fligts at your location at the moment!</h2>
+                            : <p>Loading...</p>
+                        : <h2>Server error!</h2>
+                    : <div>
+                        <h2>Geolocation is not allowed! Please enable geolocation to continue!</h2>
+                        <a href="http://waziggle.com/BrowserAllow.aspx" target="_blank" rel="noopener noreferrer">How to enable geolocation?</a>
+                      </div>}
             </div>
         )
     }
